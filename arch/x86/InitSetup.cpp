df85ee6fbbec77b235fddef3dc31f74c0c7bf1ff
Cristian Bidea <cristian.bidea@gmail.com>
2011-09-17 12:09:20 +0300

    More defines

--- arch/x86/InitSetup.cpp
+++ arch/x86/InitSetup.cpp
@@ -7,6 +7,39 @@
 #include <phos/InitSetup.hpp>
 #include <phos/Video.hpp>
 
+#define IRQ0	0
+#define IRQ1	1
+#define IRQ2	2
+#define IRQ3	3
+#define IRQ4	4
+#define IRQ5	5
+#define IRQ6	6
+#define IRQ7	7
+#define IRQ8	8
+#define IRQ9	9
+#define IRQ10	10
+#define IRQ11	11
+#define IRQ12	12
+#define IRQ13	13
+#define IRQ14	14
+#define IRQ15	15
+#define IRQ16	16
+#define IRQ17	17
+#define IRQ18	18
+#define IRQ19	19
+#define IRQ20	20
+#define IRQ21	21
+#define IRQ22	22
+#define IRQ23	23
+#define IRQ24	24
+#define IRQ25	25
+#define IRQ26	26
+#define IRQ27	27
+#define IRQ28	28
+#define IRQ29	29
+#define IRQ30	30
+#define IRQ31	31
+
 //---------------------------------------------------------------------------
 //    Local vars
 //---------------------------------------------------------------------------
@@ -107,38 +140,38 @@ static void init_idt()
 		idt[i].flags = 0;
 	}
 
-	idt_set_entry( 0, (u32)isr0,  0x08, 0x8E);
-	idt_set_entry( 1, (u32)isr1,  0x08, 0x8E);
-	idt_set_entry( 2, (u32)isr2,  0x08, 0x8E);
-	idt_set_entry( 3, (u32)isr3,  0x08, 0x8E);
-	idt_set_entry( 4, (u32)isr4,  0x08, 0x8E);
-	idt_set_entry( 5, (u32)isr5,  0x08, 0x8E);
-	idt_set_entry( 6, (u32)isr6,  0x08, 0x8E);
-	idt_set_entry( 7, (u32)isr7,  0x08, 0x8E);
-	idt_set_entry( 8, (u32)isr8,  0x08, 0x8E);
-	idt_set_entry( 9, (u32)isr9,  0x08, 0x8E);
-	idt_set_entry(10, (u32)isr10, 0x08, 0x8E);
-	idt_set_entry(11, (u32)isr11, 0x08, 0x8E);
-	idt_set_entry(12, (u32)isr12, 0x08, 0x8E);
-	idt_set_entry(13, (u32)isr13, 0x08, 0x8E);
-	idt_set_entry(14, (u32)isr14, 0x08, 0x8E);
-	idt_set_entry(15, (u32)isr15, 0x08, 0x8E);
-	idt_set_entry(16, (u32)isr16, 0x08, 0x8E);
-	idt_set_entry(17, (u32)isr17, 0x08, 0x8E);
-	idt_set_entry(18, (u32)isr18, 0x08, 0x8E);
-	idt_set_entry(19, (u32)isr19, 0x08, 0x8E);
-	idt_set_entry(20, (u32)isr20, 0x08, 0x8E);
-	idt_set_entry(21, (u32)isr21, 0x08, 0x8E);
-	idt_set_entry(22, (u32)isr22, 0x08, 0x8E);
-	idt_set_entry(23, (u32)isr23, 0x08, 0x8E);
-	idt_set_entry(24, (u32)isr24, 0x08, 0x8E);
-	idt_set_entry(25, (u32)isr25, 0x08, 0x8E);
-	idt_set_entry(26, (u32)isr26, 0x08, 0x8E);
-	idt_set_entry(27, (u32)isr27, 0x08, 0x8E);
-	idt_set_entry(28, (u32)isr28, 0x08, 0x8E);
-	idt_set_entry(29, (u32)isr29, 0x08, 0x8E);
-	idt_set_entry(30, (u32)isr30, 0x08, 0x8E);
-	idt_set_entry(31, (u32)isr31, 0x08, 0x8E);
+	idt_set_entry(IRQ0,  (u32)isr0,  0x08, 0x8E);
+	idt_set_entry(IRQ1,  (u32)isr1,  0x08, 0x8E);
+	idt_set_entry(IRQ2,  (u32)isr2,  0x08, 0x8E);
+	idt_set_entry(IRQ3,  (u32)isr3,  0x08, 0x8E);
+	idt_set_entry(IRQ4,  (u32)isr4,  0x08, 0x8E);
+	idt_set_entry(IRQ5,  (u32)isr5,  0x08, 0x8E);
+	idt_set_entry(IRQ6,  (u32)isr6,  0x08, 0x8E);
+	idt_set_entry(IRQ7,  (u32)isr7,  0x08, 0x8E);
+	idt_set_entry(IRQ8,  (u32)isr8,  0x08, 0x8E);
+	idt_set_entry(IRQ9,  (u32)isr9,  0x08, 0x8E);
+	idt_set_entry(IRQ10, (u32)isr10, 0x08, 0x8E);
+	idt_set_entry(IRQ11, (u32)isr11, 0x08, 0x8E);
+	idt_set_entry(IRQ12, (u32)isr12, 0x08, 0x8E);
+	idt_set_entry(IRQ13, (u32)isr13, 0x08, 0x8E);
+	idt_set_entry(IRQ14, (u32)isr14, 0x08, 0x8E);
+	idt_set_entry(IRQ15, (u32)isr15, 0x08, 0x8E);
+	idt_set_entry(IRQ16, (u32)isr16, 0x08, 0x8E);
+	idt_set_entry(IRQ17, (u32)isr17, 0x08, 0x8E);
+	idt_set_entry(IRQ18, (u32)isr18, 0x08, 0x8E);
+	idt_set_entry(IRQ19, (u32)isr19, 0x08, 0x8E);
+	idt_set_entry(IRQ20, (u32)isr20, 0x08, 0x8E);
+	idt_set_entry(IRQ21, (u32)isr21, 0x08, 0x8E);
+	idt_set_entry(IRQ22, (u32)isr22, 0x08, 0x8E);
+	idt_set_entry(IRQ23, (u32)isr23, 0x08, 0x8E);
+	idt_set_entry(IRQ24, (u32)isr24, 0x08, 0x8E);
+	idt_set_entry(IRQ25, (u32)isr25, 0x08, 0x8E);
+	idt_set_entry(IRQ26, (u32)isr26, 0x08, 0x8E);
+	idt_set_entry(IRQ27, (u32)isr27, 0x08, 0x8E);
+	idt_set_entry(IRQ28, (u32)isr28, 0x08, 0x8E);
+	idt_set_entry(IRQ29, (u32)isr29, 0x08, 0x8E);
+	idt_set_entry(IRQ30, (u32)isr30, 0x08, 0x8E);
+	idt_set_entry(IRQ31, (u32)isr31, 0x08, 0x8E);
 
 	asm_init_idt((u32)&idt_p);
 }
